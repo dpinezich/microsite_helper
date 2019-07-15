fed0b3dd51523936b411f4f116e3f5bb7c23ad98
dpinezich <david.pinezich@gmail.com>
2019-07-15 09:55:06 +0200

    added more details

--- cleanup.py
+++ cleanup.py
@@ -5,17 +5,29 @@ import datetime as dt
 from collections import OrderedDict
 import operator
 
+# Number of not accepted entries (not validated)
+not_accepted = 0
 
-import time
+# Number of corrected entries
+corrected_pieces = 0
 
-notAccepted = 0
-correctedPieces = 0
+# Number of deleted entries
+deleted_pieces = 0
+
+# full number of entries
 full_number = 0
+
+# dict of validated timestamps
 timestamps_validated = {}
+
+# names dict
 names_dict = {}
 
+# open the main db
 with open('db.json', encoding='utf-8') as json_file:
+    # read the db.json
     dataset = json.load(json_file)
+    newDataset = {}
 
 
     for entry in dataset:
@@ -23,40 +35,40 @@ with open('db.json', encoding='utf-8') as json_file:
         subject = dataset[entry].get("subject",None)
         if(subject != None):
             del(dataset[entry]['subject'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         textBody = dataset[entry].get("textBody", None)
         if (textBody != None):
             del (dataset[entry]['textBody'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         htmlBody = dataset[entry].get("htmlBody", None)
         if (htmlBody != None):
             del (dataset[entry]['htmlBody'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         original_gender = dataset[entry].get("original_gender", None)
         if (original_gender != None):
             del (dataset[entry]['original_gender'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         salutation = dataset[entry].get("salutation", None)
         if (salutation != None):
             del (dataset[entry]['salutation'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         ip_address = dataset[entry].get("ip_address", None)
         if (ip_address != None):
             del (dataset[entry]['ip_address'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         link = dataset[entry].get("link", None)
         if (link != None):
             del (dataset[entry]['link'])
-            correctedPieces += 1
+            corrected_pieces += 1
 
         if(dataset[entry]['validated'] != 1):
-            notAccepted = notAccepted + 1
+            not_accepted = not_accepted + 1
 
         if(dataset[entry]['validated'] == 1):
             dt_object = str(dt.date.fromtimestamp(dataset[entry]['confirmation_time']/1000.0))
@@ -65,14 +77,19 @@ with open('db.json', encoding='utf-8') as json_file:
             else:
                 timestamps_validated[dt_object] = 0
 
-        ### names dict
-        name = dataset[entry]['first_name'] + ' ' + dataset[entry]['last_name']
-        if (name in names_dict):
-            names_dict[name] = names_dict[name] + 1
+        if(dataset[entry]['email'] == 'christian@shootclever.com'):
+            deleted_pieces += 1
         else:
-            names_dict[name] = 0
 
-    json.dump(dataset, open("test.json", "w"), ensure_ascii=False)
+            newDataset[entry] = dataset[entry]
+            ### names dict
+            name = newDataset[entry]['first_name'] + ' ' + newDataset[entry]['last_name']
+            if (name in names_dict):
+                names_dict[name] = names_dict[name] + 1
+            else:
+                names_dict[name] = 0
+
+    json.dump(newDataset, open("test.json", "w"), ensure_ascii=False)
 
 
 duplicates = 0
@@ -97,11 +114,12 @@ plt.grid(True)
 plt.show()
 
 
-print("Number of not accepted " + str(notAccepted))
+print("Number of not accepted " + str(not_accepted))
 print("Number of items " + str(full_number))
-print("Percentage not accepted " + str(notAccepted / (full_number / 100)))
+print("Percentage not accepted " + str(not_accepted / (full_number / 100)))
 
-print("Total of valid entries " + str(full_number - notAccepted))
+print("Total of valid entries " + str(full_number - not_accepted))
 
 
-print("Number of corrected " + str(correctedPieces))
+print("Number of corrected " + str(corrected_pieces))
+print("Number of deleted " + str(deleted_pieces))
